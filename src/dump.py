e0f5bb0dde921047c4e3a38a61334a1b9d3b5921
Harmen Stoppels <me@harmenstoppels.nl>
2024-03-27 12:34:10 +0100

    add dump.py

---
+++ src/dump.py
@@ -0,0 +1,39 @@
+import io
+import sqlite3
+import sys
+
+import spack.repo
+from packaging.version import Version
+from spack.build_systems.python import PythonPackage
+
+# db
+conn = sqlite3.connect("data.db")
+c = conn.cursor()
+
+
+def is_pypi(pkg):
+    if not PythonPackage in pkg.__bases__:
+        return False
+    name = pkg.name[3:] if pkg.name.startswith("py-") else pkg.name
+    return c.execute("SELECT * FROM versions WHERE name = ?", (name,)).fetchone() is not None
+
+
+def dump(f: io.StringIO = sys.stdout):
+    count = 0
+    for pkg in spack.repo.PATH.all_package_classes():
+        if not is_pypi(pkg):
+            continue
+        count += 1
+        name = pkg.name[3:] if pkg.name.startswith("py-") else pkg.name
+        for version in pkg.versions:
+            try:
+                Version(str(version))
+            except:
+                print(f"skipping {name}=={version}")
+                continue
+            print(f"{name}=={version}", file=f)
+    print(f"total: {count} pypi packages")
+
+
+with open("spack_requirements.txt", "w") as f:
+    dump(f)
